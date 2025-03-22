b94bf412fa349ba6b2b0f1c8e9420b8f953aef1e
vinitanwar <125426162+vinitanwar@users.noreply.github.com>
2025-03-22 19:12:09 +0530

    once mee

--- app/layout.js
+++ app/layout.js
@@ -73,8 +73,9 @@ export default async function RootLayout({ children }) {
             `,
           }}
         />
+        
 
-        {/* Google Tag Manager - Head */}
+      
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -86,7 +87,27 @@ export default async function RootLayout({ children }) {
             `,
           }}
         />
+
+<script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '905109698255955');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
+
 
      
       <body className={inter.className}>
@@ -97,6 +118,16 @@ export default async function RootLayout({ children }) {
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
+        </noscript>
+
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=905109698255955&ev=PageView&noscript=1"
+          />
         </noscript>
 
      
