8160958381320103ab53e39e4b624a5bb037a4a6
vivek <157991083+jonty1231@users.noreply.github.com>
2025-04-15 15:10:43 +0530

    apis

--- app/Component/common/index.js
+++ app/Component/common/index.js
@@ -1,5 +1,5 @@
 // export const apilink = 'http://localhost:8000/api/v1'
-export const apilink = 'http://localhost:8000/api/v1'
+export const apilink = 'https://api.jasskaran.in/api/v1'
 //https://admin.gursimran.online
 export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
 export const storageLink = 'https://api.jasskaran.in/storage'
