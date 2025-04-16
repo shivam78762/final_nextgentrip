461549a40128ef082e7954a17254050e4591c3e7
vivek <157991083+jonty1231@users.noreply.github.com>
2025-04-16 10:45:06 +0530

    git add links

--- app/Component/common/index.js
+++ app/Component/common/index.js
@@ -1,8 +1,8 @@
 // export const apilink = 'http://localhost:8000/api/v1'
-export const apilink = 'http://localhost:8000/api/v1'
-//https://admin.gursimran.online
-export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
-export const storageLink = 'http://localhost:8000/storage'
+export const apilink = 'https://api.jasskaran.in/api/v1'
 
+export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
+export const storageLink = 'https://api.jasskaran.in/storage'
+ 
 export const development= "roduction";
 //http://localhost:8000
