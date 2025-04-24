31a59041f368a1f43525f7523be43af7742d78d9
manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-04-24 11:48:33 +0530

    add-news-uploads

--- app/Component/common/index.js
+++ app/Component/common/index.js
@@ -1,8 +1,8 @@
-// export const apilink = 'http://localhost:8000/api/v1'
-export const apilink = 'https://vivek.futuretouch.org/api/v1'
+// export const apilink = 'http://api.tringaa.com//api/v1'
+export const apilink = 'https://api.tringaa.com/api/v1'
 
 export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
-export const storageLink = 'https://vivek.futuretouch.org/storage'
+export const storageLink = 'https://api.tringaa.com/storage'
  
 export const development= "roduction";
-//http://localhost:8000
+//http://api.tringaa.com/
