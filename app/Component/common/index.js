f57252bbe39a39bc9bcec0ba04ce8bb5b71f7f37
vivek <157991083+jonty1231@users.noreply.github.com>
2025-05-02 15:36:27 +0530

    add gitcomit

--- app/Component/common/index.js
+++ app/Component/common/index.js
@@ -1,4 +1,4 @@
-export const apilink = 'http://api.tringaa.com//api/v1'
+export const apilink = 'http://api.tringaa.com/api/v1'
 // export const apilink = 'http://127.0.0.1:8000/api/v1'
 
 export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
