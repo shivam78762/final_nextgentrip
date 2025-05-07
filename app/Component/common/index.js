e62d8468ec5fc1867b8364fcfa9e4f56f7ed29db
manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-05-07 12:05:00 +0530

    add-production-keys

--- app/Component/common/index.js
+++ app/Component/common/index.js
@@ -1,9 +1,9 @@
-export const apilink = 'http://148.135.137.54:8000/api/v1'
+export const apilink = 'http://admin.nextgentrip.com/api/v1'
 // export const apilink = 'http://api.tringaa.com/api/v1'
 
 export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
 // export const storageLink = 'http://api.tringaa.com/storage'
-export const storageLink = 'http://148.135.137.54:8000/storage'
+export const storageLink = 'http://admin.nextgentrip.com/storage'
  
 export const development= "roduction";
 //https://api.tringaa.com/  
