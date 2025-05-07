56368722be9384f95a748faf7a5011d17631c645
manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-05-07 12:10:12 +0530

    addd-s

--- app/Component/common/index.js
+++ app/Component/common/index.js
@@ -1,9 +1,9 @@
-export const apilink = 'http://admin.nextgentrip.com/api/v1'
+export const apilink = 'https://admin.nextgentrip.com/api/v1'
 // export const apilink = 'http://api.tringaa.com/api/v1'
 
 export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
 // export const storageLink = 'http://api.tringaa.com/storage'
-export const storageLink = 'http://admin.nextgentrip.com/storage'
+export const storageLink = 'https://admin.nextgentrip.com/storage'
  
 export const development= "roduction";
 //https://api.tringaa.com/  
